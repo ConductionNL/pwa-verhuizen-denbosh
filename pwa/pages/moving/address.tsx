e5b76aad1df367d8dac3c40bcf054a7b67e67bf4
Sarai Misidjan <sarai@conduction.nl>
2021-10-18 15:06:21 +0200

    added error message addresses

--- pwa/pages/moving/address.tsx
+++ pwa/pages/moving/address.tsx
@@ -3,22 +3,15 @@ import React, {ReactNode, useEffect, useState} from "react";
 import Link from 'next/link'
 import Layout from "../../components/common/layout";
 import Grid from "@mui/material/Grid";
-import ActionMenu from "../../components/common/actionmenu";
-import Hidden from "@mui/material/Hidden";
-import PageHeader from "../../components/common/pageheader";
 import {Tab, Tabs, Typography, Box, TextField, Avatar} from "@mui/material";
-import PaperCard from "../../components/common/paperCard";
 import {useRouter} from "next/router";
 import Stepper from "../../components/moving/stepper";
 import makeStyles from "@mui/styles/makeStyles";
 import {ChevronLeft, ChevronRight} from "@mui/icons-material";
-import {useGet, useMutate} from "restful-react";
-import {useUserContext} from "../../components/context/userContext";
 import {useAppContext} from "../../components/context/state";
-import {type} from "os";
 import SearchIcon from '@mui/icons-material/Search';
-import {ForwardRounded} from "@material-ui/icons";
 import {updateRequest} from "../../components/utility/RequestHandler";
+import WarningIcon from '@mui/icons-material/Warning';
 
 const useStyles = makeStyles((theme) => ({
   inputStyle: {
@@ -42,6 +35,10 @@ export default function Address() {
   const [houseNumberInputError, setHouseNumberInputError] = useState(false);
   const [houseNumberInputHelperText, setHouseNumberInputHelperText] = useState('');
 
+  const [errorMessageText, setErrorMessageText] = useState('');
+  const [errorMessageTitle, setErrorMessageTitle] = useState('');
+  const [icon, setIcon] = useState(false);
+
   let context = useAppContext();
   const router = useRouter();
 
@@ -55,6 +52,9 @@ export default function Address() {
     setPostalCodeInputHelperText('');
     setHouseNumberInputError(false);
     setHouseNumberInputHelperText('');
+    setIcon(false)
+    setErrorMessageTitle("")
+    setErrorMessageText("")
 
     if (postalCodeInput.value.length == 0) {
       valid = false;
@@ -90,15 +90,15 @@ export default function Address() {
 
       fetch(context.apiUrl + "/gateways/as/adressen?postcode=" + postalCode + '&huisnummer=' + houseNumber + "&huisnummertoevoeging=" + suffix, {
         credentials: 'include',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {'Content-Type': 'application/json'},
       })
         .then(response => response.json())
-        .then((data) =>  {
-            setResults(data);
+        .then((data) => {
+          setResults(data);
         });
 
-      }
     }
+  }
 
   const classes = useStyles();
 
@@ -107,6 +107,13 @@ export default function Address() {
   }
 
   const processAddress = (item) => {
+
+    if (item.woonplaats !== '\'s-Hertogenbosch') {
+      setIcon(true)
+      setErrorMessageTitle("Let op!")
+      setErrorMessageText("Het nieuwe adres dat je opgeeft ligt helaas niet in een gemeente die deze service gebruikt. Geef de adreswijziging door op de website van de gemeente waar je naar toe verhuist.")
+      return;
+    }
 
     if (item.huisnummertoevoeging !== null) {
       updateRequest(
@@ -132,81 +139,94 @@ export default function Address() {
         <Stepper currentStep={0}/>
 
         <Grid item sm={12}>
-          <Typography variant="h4">
-            Wat wordt je nieuwe adres?
-          </Typography>
-          <Typography mb="10px">
-            Vul je postcode, huisnummer en eventueel toevoeging in van het nieuwe adres.
-          </Typography>
+          <Grid item sm={12}>
+            <Typography variant="h4">
+              Wat wordt je nieuwe adres?
+            </Typography>
+            <Typography mb="10px">
+              Vul je postcode, huisnummer en eventueel toevoeging in van het nieuwe adres.
+            </Typography>
+          </Grid>
+          <Grid item sm={12} style={{marginTop: 20}}>
+            <TextField
+              id="postalCode"
+              label="Postcode"
+              required
+              variant="outlined"
+              className={classes.inputStyle}
+              error={postalCodeInputError}
+              helperText={postalCodeInputHelperText}
+            />
+            <br/>
+            <br/>
+            <TextField
+              id="houseNumber"
+              label="Huisnummer"
+              required
+              variant="outlined"
+              className={classes.inputStyle}
+              error={houseNumberInputError}
+              helperText={houseNumberInputHelperText}
+            />
+            <br/>
+            <br/>
+            <TextField id="houseNumberSuffix" label="Huisnummertoevoeging" variant="outlined"
+                       className={classes.inputStyle}/>
+            <br/>
+            <br/>
 
-          <TextField
-            id="postalCode"
-            label="Postcode"
-            required
-            variant="outlined"
-            className={classes.inputStyle}
-            error={postalCodeInputError}
-            helperText={postalCodeInputHelperText}
-          />
-          <br/>
-          <br/>
-          <TextField
-            id="houseNumber"
-            label="Huisnummer"
-            required
-            variant="outlined"
-            className={classes.inputStyle}
-            error={houseNumberInputError}
-            helperText={houseNumberInputHelperText}
-          />
-          <br/>
-          <br/>
-          <TextField id="houseNumberSuffix" label="Huisnummertoevoeging" variant="outlined"
-                     className={classes.inputStyle}/>
-          <br/>
-          <br/>
-
-          <Button color="primary" onClick={handleAddress} sx={{marginBottom: "20px"}} type="button" variant="contained" endIcon={<SearchIcon/>}>Zoeken</Button>
-
-
+            <Button color="primary" onClick={handleAddress} sx={{marginBottom: "20px"}} type="button"
+                    variant="contained"
+                    endIcon={<SearchIcon/>}>Zoeken</Button>
+          </Grid>
+        </Grid>
+        <Grid item sm={12}>
           <Typography variant="h5">
             Gevonden adressen
           </Typography>
           <Typography mb="10px">
-            Staat uw adres niet in de lijst, controleer dan de ingevulde postcode, huisnummer en eventueel huisnummertoevoeging en probeer opnieuw.
+            Staat uw adres niet in de lijst, controleer dan de ingevulde postcode, huisnummer en eventueel
+            huisnummertoevoeging en probeer opnieuw.
           </Typography>
 
           <div>
             {
               results !== undefined && results !== null && results.adressen !== undefined &&
-                results.adressen.map((result) => (
-                  <Grid sx={{marginBottom: "5px"}}>
-                    <Button
-                      onClick={() => {processAddress(result)}}
-                      color="primary" type="button" variant="contained" endIcon={<ChevronRight/>}>
-                      {result.straat + " " + result.huisnummer}
-                      {result.huisnummertoevoeging !== null && result.huisnummertoevoeging}
-                      {", " + result.postcode + " " + result.woonplaats}
-                    </Button>
-                  </Grid>
-                ))
+              results.adressen.map((result) => (
+                <Grid sx={{marginBottom: "5px"}}>
+                  <Button
+                    onClick={() => {
+                      processAddress(result)
+                    }}
+                    color="primary" type="button" variant="contained" endIcon={<ChevronRight/>}>
+                    {result.straat + " " + result.huisnummer}
+                    {result.huisnummertoevoeging !== null && result.huisnummertoevoeging}
+                    {", " + result.postcode + " " + result.woonplaats}
+                  </Button>
+                </Grid>
+              ))
             }
           </div>
-
-          <Grid
-            sx={{marginTop: '30px'}}
-            justifyContent="space-between" // Add it here :)
-            container>
-            <Grid item>
-              <Link href="/moving/moving">
-                <Button variant="text" startIcon={<ChevronLeft/>}> Ga terug</Button>
-              </Link>
-            </Grid>
-           </Grid>
-
         </Grid>
-      </Grid>
+        <Grid item xs={2} sm={2} md={2} style={{marginTop: 20}}>
+          <div>{icon ? <WarningIcon color="warning" fontSize="large"/> : null}</div>
+        </Grid>
+        <Grid item xs={10} sm={10} md={10}>
+          <Typography variant="h5">{errorMessageTitle}</Typography>
+          <div>{errorMessageText}</div>
+        </Grid>
+        <Grid
+          sx={{marginTop: '30px'}}
+          justifyContent="space-between" // Add it here :)
+          container>
+          <Grid item>
+            <Link href="/moving/moving">
+              <Button variant="text" startIcon={<ChevronLeft/>}> Ga terug</Button>
+            </Link>
+          </Grid>
+        </Grid>
 
+      </Grid>
     </Layout>
   </>);
 }
