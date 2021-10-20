52b877b81143a05e9642751b267b15f6d96abfb5
ginokok1996 <ginokok1996@hotmail.com>
2021-10-20 10:04:44 +0200

    con-1731

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -10,23 +10,53 @@ import {useRouter} from "next/router";
 import PaperCard from "../components/common/paperCard";
 import {useUserContext} from "../components/context/userContext";
 import {useAppContext} from "../components/context/state";
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Link,
+  Typography
+} from "@mui/material";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import {ChevronRight} from "@mui/icons-material";
 
-const Welcome = () => (
-  <>
-    <Layout title="Welkom bij verhuizen 's Hertogenbosch!" description="waar kan ik deze description zien">
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={8} md={6}>
-          <PaperCard
-            title="Verhuizen"
-            description="Het doorgeven van een verhuizing."
-            link="/moving"
-            linkText="Start formulier "
-          />
+const Welcome = () => {
+  const router = useRouter();
+  return (
+    <>
+      <Layout title="Welkom bij verhuizen 's Hertogenbosch!" description="waar kan ik deze description zien">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} lg={4}>
+            <Card>
+              <CardActionArea onClick={() => {router.push('/moving')}}>
+                <CardContent sx={{textAlign: "center"}}>
+                  <div>
+                    <Avatar sx={{ backgroundColor: "#001759", marginLeft: "auto", marginRight: "auto" }}>
+                      <LocalShippingIcon />
+                    </Avatar>
+                  </div>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Verhuizen
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" sx={{marginBottom: "30px"}}>
+                    Met dit formulier bent u in staat om een verhuizing door te geven.
+                  </Typography>
+                  <Link href="/moving" sx={{marginLeft: "auto", marginRight: "auto"}}>
+                    <Button color="primary" variant="contained" endIcon={<ChevronRight/>}>Start formulier</Button>
+                  </Link>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
-    </Layout>
-  </>
-);
+      </Layout>
+    </>
+  );
+};
 
 export default Welcome;
 
