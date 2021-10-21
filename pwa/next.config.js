4ac0a16db0c58397b87b26e97c5da655e357b92a
ginokok1996 <ginokok1996@hotmail.com>
2021-10-21 16:38:57 +0200

    fix

--- pwa/next.config.js
+++ pwa/next.config.js
@@ -1,6 +1,6 @@
 let nonce = "";
 let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-for(let i = 0; i < length; i++) {
+for(let i = 0; i < possible.length; i++) {
   nonce += possible.charAt(Math.floor(Math.random() * possible.length));
 }
 
