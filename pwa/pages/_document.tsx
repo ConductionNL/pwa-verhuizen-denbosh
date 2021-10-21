7aaaa40490b4567f0898cb7325ca7968f0acd916
ginokok1996 <ginokok1996@hotmail.com>
2021-10-21 15:13:18 +0200

    csp test

--- pwa/pages/_document.tsx
+++ pwa/pages/_document.tsx
@@ -1,18 +1,55 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheets } from '@mui/styles'; // works with @mui/material/styles, if you prefer to use it.
+import { ServerStyleSheets } from '@mui/styles';
+import crypto from 'crypto';
+import { v4 } from 'uuid';
+
+const generateCsp = (): [csp: string, nonce: string] => {
+  const production = process.env.NODE_ENV === 'production';
+
+  // generate random nonce converted to base64. Must be different on every HTTP page load
+  const hash = crypto.createHash('sha256');
+  hash.update(v4());
+  const nonce = hash.digest('base64');
+
+  let csp = ``;
+  csp += `default-src 'none';`;
+  csp += `base-uri 'self';`;
+  csp += `style-src https://fonts.googleapis.com 'unsafe-inline';`; // NextJS requires 'unsafe-inline'
+  csp += `script-src 'nonce-${nonce}' 'self' ${production ? '' : "'unsafe-eval'"};`; // NextJS requires 'self' and 'unsafe-eval' in dev (faster source maps)
+  csp += `img-src https://www.logius.nl https://www.s-hertogenbosch.nl;`
+  // csp += `font-src https://fonts.gstatic.com http://localhost:3000;`;
+  // csp += `connect-src 'self' http://localhost;`;
+  csp += `font-src https://fonts.gstatic.com;`;
+  csp += `connect-src 'self';`;
+
+  // if (typeof window !== "undefined") {
+  //   if (window.location.href.includes('http://localhost')) {
+  //     csp += `font-src https://fonts.gstatic.com http://localhost:3000;`;
+  //     csp += `connect-src 'self' http://localhost;`;
+  //   } else {
+  //     csp += `font-src https://fonts.gstatic.com;`;
+  //     csp += `connect-src 'self';`;
+  //   }
+  // }
+
+  return [csp, nonce];
+};
 
 export default class MyDocument extends Document {
   render() {
+    const [csp, nonce] = generateCsp();
     return (
       <Html lang="en">
-        <Head>
+        <Head nonce={nonce}>
+          <meta property='csp-nonce' content={nonce} />
+          <meta httpEquiv='Content-Security-Policy' content={csp} />
           {/* Not exactly required, but this is the PWA primary color */}
           <meta name="theme-color"/>
         </Head>
         <body>
         <Main />
-        <NextScript />
+        <NextScript nonce={nonce} />
         </body>
       </Html>
     );
